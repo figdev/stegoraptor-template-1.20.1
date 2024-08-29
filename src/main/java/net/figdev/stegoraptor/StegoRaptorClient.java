8f38aacd226d56a139ae919d257955b4fa6749e8
fig <avollxxiv@gmail.com>
2024-08-29 18:12:20 -0400

    video #1 completed!

---
+++ src/main/java/net/figdev/stegoraptor/StegoRaptorClient.java
@@ -0,0 +1,10 @@
+package net.figdev.stegoraptor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StegoRaptorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
