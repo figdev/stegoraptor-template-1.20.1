94020d08fff7381f292c5e1f41b09483f529f959
fig <avollxxiv@gmail.com>
2024-08-29 17:50:43 -0400

    Initial commit

---
+++ src/main/java/net/figdev/stegoraptor/StegoRaptorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.figdev.stegoraptor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StegoRaptorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
