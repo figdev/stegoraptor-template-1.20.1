f5e6769690edeb2fff1cb380375e63367fe01110
fig <avollxxiv@gmail.com>
2024-08-31 18:12:03 -0400

    video #4 completed!

--- src/main/java/net/figdev/stegoraptor/block/ModBlocks.java
+++ src/main/java/net/figdev/stegoraptor/block/ModBlocks.java
@@ -5,18 +5,30 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.figdev.stegoraptor.StegoRaptor;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBLock("ruby_block",
         new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBLock("raw_ruby_block",
         new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RUBY_ORE = registerBLock("ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(2, 5)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBLock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4f), UniformIntProvider.create(2, 5)));
+    public static final Block NETHER_RUBY_ORE = registerBLock("nether_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(1.5f), UniformIntProvider.create(2, 5)));
+    public static final Block END_STONE_RUBY_ORE = registerBLock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE).strength(4f), UniformIntProvider.create(2, 5)));
+
 
     private static Block registerBLock(String name, Block block) {
         registerBlockItem(name, block);
